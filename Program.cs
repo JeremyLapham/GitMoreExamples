cd5eb6067984d5562bbff2ab283c2238b98381ca
JeremyLapham <jeremylapham2004@gmail.com>
2022-10-17 11:41:48 -0700

    Letting people know about new cut

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(2+2);
+
+//I will make new projects 
+
+Console.WriteLine("I like amrs new Cut");
